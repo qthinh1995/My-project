b9ce4fd4ba4a62f9cacf8d5990b37073b881ace0
tuan nguyen <nttuan1995@gmail.com>
2018-07-14 08:55:01 +0700

    add simulator

--- app/components/layout-IBM-Watson-POC/caroGame.jsx
+++ app/components/layout-IBM-Watson-POC/caroGame.jsx
@@ -1,16 +1,17 @@
 import React, { Component, PropTypes } from 'react'
 import connect from 'connect-alt'
 // import CaroSquare from './CaroSquare'
-import { get, values, isEmpty, filter } from 'lodash'
+import { get, set, values, filter, cloneDeep } from 'lodash'
 
 const arrayMap = Array(20)
 for (let i = 0; i < arrayMap.length; i++) {
     arrayMap[i] = Array(40).fill(null)
 }
-let indexObj = 0
-const objectMap = {}
 let nearbyPoints = []
+const futureNearbyPoints = {}
+const squareChecked = []
 const nebourArr = [ 'dlt', 'ht', 'vl', 'dlb', 'drt', 'vr', 'drb', 'hb' ];
+let i = 0
 
 @connect(({ requests: { inProgress }, session: { session } }) => ({ inProgress, session }))
 
@@ -31,56 +32,86 @@ export default class CaroGame extends Component {
     onClickSquare({ x, y }) {
         const { caroMap, isClickX } = this.state;
         const ftype = isClickX ? 'X' : 'O';
-        if (!caroMap[y][x]) {
-            caroMap[y][x] = ftype;
-            objectMap[indexObj] = { x, y}
-            indexObj++
-            console.log('indexObj', objectMap)
-		}
+        if (!get(caroMap, `[${y}][${x}].value`)) {
+            set(caroMap, `[${y}][${x}].value`, ftype);
+            squareChecked.push({ x, y, value: ftype })
+        }
 		
-        const hasWinner = this.checkWin({ x, y });
+        // const hasWinner = this.checkWin({ x, y });
         const border = {};
         nebourArr.forEach(dir => {
             border[dir] = this.checkNebour({ x, y, ftype, dir });
         });
         const value = values(border)
 
-        this.positionNearly({ value, x, y, caroMap })
-        this.checkWin2({ border })
-        this.setState({ caroMap, isClickX: !isClickX, hasWinner })
+        nearbyPoints = this.positionNearly({ value, x, y, caroMap })
+        // this.checkWin2({ border })
+        this.setState({ caroMap, isClickX: !isClickX })
     }
 
 	// point: { direction: 'h' (horizontal) / 'v' (vertical) / 'dl' (diagonal left) / 'dr' (diagonal right)}
-	checkWin2({ border = {} }) {
-        const score = {
-            h: get(border, 'hb.y') - get(border, 'ht.y') - 1,
-            v: get(border, 'vr.x') - get(border, 'vl.x') - 1,
-            dl: get(border, 'drb.x') - get(border, 'dlt.x') - 1,
-            dr: get(border, 'drt.x') - get(border, 'dlb.x') - 1
-        }        
-        console.log('score:', score, border, nearbyPoints);
-    }
+	// checkWin2({ border = {} }) {
+    //     const score = {
+    //         h: get(border, 'hb.y') - get(border, 'ht.y') - 1,
+    //         v: get(border, 'vr.x') - get(border, 'vl.x') - 1,
+    //         dl: get(border, 'drb.x') - get(border, 'dlt.x') - 1,
+    //         dr: get(border, 'drt.x') - get(border, 'dlb.x') - 1
+    //     }        
+    //     console.log('score:', score, border);
+    // }
     
     positionNearly({ value = [], x = '', y= '', caroMap }) {
-        if (isEmpty(nearbyPoints)) {
-            nearbyPoints = value
-        } else {
-            nearbyPoints = filter(nearbyPoints, o => {
-                return !(o.x === x && o.y ===y)
-            })
-            value.forEach((item) => {
-                if (caroMap[item.y][item.x]) {
-                    return
-                }
-                const isContain = isEmpty(filter(nearbyPoints, item))
-                nearbyPoints = isContain ? nearbyPoints.concat(item) : nearbyPoints
-            })
-        }
+        set(caroMap, `[${y}][${x}].isNear`, false);
+        let arrNearPoints = cloneDeep(nearbyPoints)
+        // remove location has been click
+        arrNearPoints = filter(arrNearPoints, o => {
+            return !(o.x === x && o.y ===y)
+        })
+        value.forEach(item => {
+            if (!get(caroMap, `[${item.y}][${item.x}].isNear`) && !get(caroMap, `[${item.y}][${item.x}].value`)) {
+                set(caroMap, `[${item.y}][${item.x}].isNear`, true);
+                arrNearPoints.push({ x: item.x, y: item.y })
+            }
+        })
+        return arrNearPoints
     }
+
+    simulator() {
+        const { isClickX } = this.state;
+        const caroMap = cloneDeep(this.state.caroMap)
+        const ftype = isClickX ? 'X' : 'O';
+        // nearbyPoints.forEach(item => {
+        //     const border = {};
+        //     nebourArr.forEach(dir => {
+        //         border[dir] = this.checkNebour({ x: item.x, y: item.y, ftype, dir });
+        //     });
+
+        //     const value = values(border)
+
+        //     this.positionNearly({ value, x: item.x, y: item.y, caroMap })
+        // })
+        const item = nearbyPoints[i]
+        const border = {};
+        nebourArr.forEach(dir => {
+            border[dir] = this.checkNebour({ x: item.x, y: item.y, ftype, dir });
+        });
+
+        const value = values(border)
+
+        futureNearbyPoints[i] = this.positionNearly({ value, x: item.x, y: item.y, caroMap })
+        i++;
+        console.log(futureNearbyPoints)
+    }
+
+    // simulatorAI() {
+    //     nearbyPoints.forEach(item => {
+
+    //     })
+    // }
 
 	checkNebour({ x, y, ftype = 'X', dir }) {
 		const { caroMap } = this.state;
-		
+        
         if ([ 'dlt', 'vl', 'dlb' ].indexOf(dir) > -1) { x--; }
         if ([ 'drt', 'vr', 'drb' ].indexOf(dir) > -1) { x++; }
         if ([ 'dlt', 'ht', 'drt' ].indexOf(dir) > -1) { y--; }
@@ -88,10 +119,10 @@ export default class CaroGame extends Component {
 
         if (x < 0 || y < 0) { return { x, y } }
 
-		if (caroMap[y][x] === ftype) {
+		if (get(caroMap, `[${y}][${x}].value`) === ftype) {
 			return this.checkNebour({ x, y, ftype, dir });
-		}
-
+        }
+        
 		return { x, y };
 	}
 
@@ -150,27 +181,23 @@ export default class CaroGame extends Component {
 
     render() {
         const { caroMap, hasWinner, isClickX } = this.state;
-        console.log('nearbyPoints', nearbyPoints)
         return (
             <div className="caro-board">
                 {!hasWinner &&<h2>It's turn: {isClickX ? 'X' : 'O'}</h2>}
                 {hasWinner && <h2>The winner is {hasWinner}</h2>}
+                <button onClick={() => this.simulator()}> Click </button>
                 {caroMap.map((row, y) => {
                     return (
                         <div className="row" key={y} >
                             {row.map((square, x) => {
-                                let isNear = false
-                                nearbyPoints.forEach((item) => {
-                                    if (item.x === x && item.y === y) {
-                                        isNear = true
-                                    }
-                                })
+                                const isNear = !!get(square, 'isNear')
+                                const value = get(square, 'value')
                                 return (
 									<div 
 										className={`square ${isNear ? 'near-square' : ''} `}
 										key={ `${x}-${y}` } 
-										onClick={ square ? '' : () => this.onClickSquare({x, y})}>
-											{ square }
+										onClick={ value ? '' : () => this.onClickSquare({x, y})}>
+											{ value }
 									</div>
                                 )
                             })}
