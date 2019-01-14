3f5b37c928cd45b16a189fa0df8179e2913514b0
Thinh Tran <qthinh1995@gmail.com>
2019-01-14 11:55:56 +0700

    Update drag drop chat board

--- app/components/layout-IBM-Watson-POC/Chat.jsx
+++ app/components/layout-IBM-Watson-POC/Chat.jsx
@@ -188,13 +188,15 @@ export default class Chat extends Component {
 
     onResizeChatBoard(e) {
         if (!isEmpty(mousePosition)) {
-            const newPosition = { y: e.screenY };
+            const newPosition = { x: e.screenX, y: e.screenY };
             const chatBoard = document.querySelector('#chatBoard');
-            let { height } = chatBoardState;
+            let { height, width } = chatBoardState;
 
             height = height - newPosition.y + mousePosition.y;
+            width = width + newPosition.x - mousePosition.x;
 
             chatBoard.style.height = height + 'px';
+            chatBoard.style.width = width + 'px';
         }
     }
 
